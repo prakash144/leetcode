3df234433b7ab38858a97dbd12b0d80fdff9b534
Prakash Rabidas <prakash.rabidas1@gmail.com>
2024-03-28 13:51:18 +0530

    Time: 691 ms (85.20%), Space: 64.7 MB (60.86%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -2,19 +2,14 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res, comb = [], []
         
-        def backtrack(i):
+        def backtrack(start):
             if len(comb) == k:
                 res.append(comb.copy())
                 return
-            if i > n:
-                return 
-            
-            # decicion to include i
-            comb.append(i)
-            backtrack(i+1)
-            # decicion not to include i
-            comb.pop()
-            backtrack(i+1)
+            for i in range(start, n+1):
+                comb.append(i)
+                backtrack(i+1)
+                comb.pop()
             
         backtrack(1)
         return res
