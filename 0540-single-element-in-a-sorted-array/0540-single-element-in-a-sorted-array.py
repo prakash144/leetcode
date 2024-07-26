8bcaeadcc3ffbc76fe90df08f0453c89f69cc3a0
Prakash Rabidas <prakash.rabidas1@gmail.com>
2024-07-27 02:40:15 +0530

    Time: 138 ms (61.88%), Space: 26.6 MB (47.21%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,20 +1,19 @@
+from typing import List
+
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
         
-        while l <= r:
-            m = l + ((r-l)//2)
+        while l < r:  # Change from 'l <= r' to 'l < r' to avoid infinite loop
+            m = l + ((r - l) // 2)
             
-            if ((m-1 < 0 or nums[m-1] != nums[m]) and 
-                (m+1 == len(nums) or nums[m] != nums[m+1])):
-                return nums[m]
-                
-            leftSize = m - 1 if nums[m-1] == nums[m] else m
-            if leftSize % 2:
-                r = m - 1
+            if (m % 2 == 1):
+                m -= 1  # Ensure m is even so it can be paired with m+1
+            
+            # If nums[m] == nums[m+1], single element must be on the right side
+            if nums[m] == nums[m + 1]:
+                l = m + 2
             else:
-                l = m + 1
-            
-            
-            
+                r = m
         
+        return nums[l]
