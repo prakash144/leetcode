649864cdbfe9a0fb3c890f4b10d6d71bb371a9e1
Prakash Rabidas <prakash.rabidas1@gmail.com>
2024-07-29 20:38:35 +0530

    Time: 44 ms (82.53%), Space: 16.6 MB (92.07%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,16 +1,19 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # O(m*n)
+        # O(m + n)
         num1Idx = {n:i for i, n in enumerate(nums1)}
         res = [-1] * len(nums1)
         
+        stack = []
+        
         for i in range(len(nums2)):
-            if nums2[i] not in num1Idx:
-                continue
-            for j in range(i+1, len(nums2)):
-                if nums2[j] > nums2[i]:
-                    idx = num1Idx[nums2[i]]
-                    res[idx] = nums2[j]
-                    break             
+            cur = nums2[i]
+            while stack and cur > stack[-1]:
+                val = stack.pop()
+                idx = num1Idx[val]
+                res[idx] = cur
+            if cur in num1Idx:
+                stack.append(cur)
+                         
         return res
         
