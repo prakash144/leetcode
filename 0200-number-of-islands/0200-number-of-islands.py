5905b8f8a65b2abd2121fcac2cd135a354d62c4b
Prakash Rabidas <prakash.rabidas1@gmail.com>
2025-04-16 22:18:57 +0530

    Time: 317 ms (11.7%), Space: 29.2 MB (5.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,26 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
         
         rows, cols = len(grid), len(grid[0])
-        visited = set()
-        islands = 0
+        visit = set()
+        island = 0
         
         def dfs(r,c):
-            if (r not in range(rows) or 
-                c not in range(cols) or
-                (r,c) in visited or
-                grid[r][c] == "0"):
-                return 
-            visited.add((r,c))
+            if (r not in range(rows) or c not in range(cols) or (r,c) in visit or grid[r][c] == "0"):
+                return
+            visit.add((r,c))
+            dfs(r,c+1)
             dfs(r+1,c)
+            dfs(r,c-1)
             dfs(r-1,c)
-            dfs(r, c+1)
-            dfs(r, c-1)
-                
-            
         
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r,c) not in visited:
+                if grid[r][c] == "1" and (r,c) not in visit:
                     dfs(r, c)
-                    islands += 1
+                    island += 1
                     
-        return islands
+        return island
